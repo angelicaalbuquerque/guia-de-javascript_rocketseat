f385f246b93a5b8b136bf00db5351207412de4e4
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-03-07 17:50:01 -0300

    Retornando valores dentro da função

---
+++ exercícios/mod-06-functions/functions-3.js
@@ -0,0 +1,11 @@
+const sum = function (number1, number2) {
+  let total = number1 + number2;
+  return total;
+};
+
+let number1 = 34;
+let number2 = 25;
+
+console.log(`O número 1 é: ${number1}`);
+console.log(`O número 2 é: ${number2}`);
+console.log(`A soma é: ${sum(number1, number2)}`);
