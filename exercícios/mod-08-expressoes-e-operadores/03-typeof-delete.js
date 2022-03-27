aed3aba014f4eba838eeb2c6bed4a885aa1285a9
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-03-27 14:28:32 -0300

    Typeof e delete

---
+++ exercícios/mod-08-expressoes-e-operadores/03-typeof-delete.js
@@ -0,0 +1,15 @@
+/* Em operadores unários, os mais comuns são o typeof e o delete, onde o typeof serve para saber o tipo de dado que temos na expressão, e o delete vai deletar alguma propriedade de um objeto, desde que ela exista.
+
+Exemplos de uso:
+/* delete 
+const person = {
+    name: 'Mayk',
+    age: 25,
+}
+delete person.age
+
+console.log(person)
+Nesse caso estamos deletando a propriedade age do objeto person.
+
+console.log(tyepof "mayk")
+Aqui vemos qual o tipo de dado que temos, nesse caso uma string. */
