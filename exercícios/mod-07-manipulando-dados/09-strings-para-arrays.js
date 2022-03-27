40cb71fb6b46eb46939528ce3c4d570fe3b49f06
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-03-27 11:20:18 -0300

    Strings para arrays

---
+++ exercícios/mod-07-manipulando-dados/09-strings-para-arrays.js
@@ -0,0 +1,6 @@
+/* Strings para arrays
+
+Para transformar uma string em um array, pode-se usar o objeto Array com o método from(), passando como argumento a string a ser transformada em array. Por exemplo console.log(Array.from("developer"). */
+
+let word = "manipulação";
+console.log(Array.from(word));
