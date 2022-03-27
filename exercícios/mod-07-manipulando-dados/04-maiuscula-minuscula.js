8880e3f9892ce167b6ec76e36f38862d89dea178
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-03-27 11:02:13 -0300

    Maiúsculas e minúsculas

---
+++ exercícios/mod-07-manipulando-dados/04-maiuscula-minuscula.js
@@ -0,0 +1,5 @@
+//Para transformar uma string em letras maiúsculas pode-se usar o método toUpperCase(), e ao contrário, também é possível usar o toLowerCase() para transformar uma string em todas letras minúsculas.
+
+let word = "Programar é Mó Barato!";
+console.log(word.toUpperCase());
+console.log(word.toLowerCase());
