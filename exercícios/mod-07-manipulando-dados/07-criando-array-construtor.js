fe5cc5889b6feb9d315e97afd9e013ac02e74ea5
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-03-27 11:14:27 -0300

    Criando array com construtor

---
+++ exercícios/mod-07-manipulando-dados/07-criando-array-construtor.js
@@ -0,0 +1,7 @@
+// Para criar um array com um método construtor, usa-se new Array(args), passando como argumento o tamanho deste vetor ou os valores, por exemplo: const array = new Array("a", "b", "c").
+
+let myArray = ["a", "b", "c"];
+console.log(myArray);
+
+let myArrayConstructor = new Array("a", "b", "c"); //cada argumento vira um elemento no array
+console.log(myArrayConstructor);
