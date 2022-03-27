ce9d318dfae28d25805580163549a848f5d76890
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-03-27 14:24:16 -0300

    New

---
+++ exercícios/mod-08-expressoes-e-operadores/02-new.js
@@ -0,0 +1,13 @@
+/* New
+
+Descrição
+A expressão new, também denominada como left-hand-side expression, serve para criar um novo objeto usando as funções construtoras que temos por padrão no JavaScript.
+
+Um exemplo de seu uso é:
+
+let date = new Date('2020-12-01')
+console.log(date.__proto__) */
+
+let name = new String("Angelica");
+let age = new Number(31);
+console.log(name, age);
