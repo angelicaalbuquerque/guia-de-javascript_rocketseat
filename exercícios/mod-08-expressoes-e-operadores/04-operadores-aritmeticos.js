89b43f0ae83183b68a888327adf439f164913454
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-03-27 14:35:48 -0300

    Operadores aritméticos

---
+++ exercícios/mod-08-expressoes-e-operadores/04-operadores-aritmeticos.js
@@ -0,0 +1,37 @@
+/*Operadores aritméticos
+
+Descrição
+Falaremos nessa aula sobre operadores aritméticos, que nos possibilitam fazer cálculos matemáticos.
+
+O operador de multiplicação é o * (asterisco);
+O operador de divisão é a / (barra);
+O operador de soma é o + (positivo);
+O operador de subtração é o - (negativo).
+
+Outros operadores aritméticos incluem o resto de divisão, incremento, decremento e exponencial, com exemplos a seguir:*/
+
+//resto da divisão: sinal %
+let remainder;
+remainder = 11 % 9;
+console.log(remainder);
+
+//incremento: sinal ++
+let increment = 0;
+
+console.log(++increment);
+console.log(increment++);
+console.log(increment);
+
+//decremento: sinal --
+let decrement = 0;
+decrement--;
+console.log(decrement);
+
+//exponencial: sinal **
+console.log(2 ** 3);
+
+console.log(3.2 * 16);
+console.log(12 / 2);
+console.log(5 + 5);
+console.log(5 - 5);
+console.log(5 % 2);
